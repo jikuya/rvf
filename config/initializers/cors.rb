c3385d74704037106ee2ed741c87bae78da78932
jikuya <jikuya@gmail.com>
2025-05-10 07:54:39 +0900

    Update CORS and S3 configurations for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins ENV.fetch('FRONTEND_URL', 'https://rvf.vercel.app')
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization']
+      expose: ['Authorization'],
+      credentials: true
   end
 end
