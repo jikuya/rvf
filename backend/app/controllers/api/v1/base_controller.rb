792b317d0801aa7faaa667d0c9a7210d960b3397
jikuya <jikuya@gmail.com>
2025-05-10 08:16:43 +0900

    Update base controller configuration

--- backend/app/controllers/api/v1/base_controller.rb
+++ backend/app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,12 @@
 module Api
   module V1
     class BaseController < ApplicationController
+      # CSRF保護のためにモジュールをインクルード
+      include ActionController::RequestForgeryProtection
+
+      # JSON API の場合は null_session が定番
       protect_from_forgery with: :null_session
+
       respond_to :json
     end
   end
