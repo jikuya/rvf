c868370923def85c94f7c7e07d9d254559fb8719
jikuya <jikuya@gmail.com>
2025-05-10 00:03:45 +0900

    Add host authorization in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative "boot"
 
-require "rails"
+require "rails/all"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
@@ -26,7 +26,7 @@ module Backend
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -40,5 +40,8 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Allow requests from Heroku domain
+    config.hosts << "rvf-f1cc04378d39.herokuapp.com"
   end
 end
