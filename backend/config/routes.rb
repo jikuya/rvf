90cf81054623dea2a6859f643c4ba2d2cc8f22e6
jikuya <jikuya@gmail.com>
2025-05-11 19:57:39 +0900

    fix: job_applications（アンダースコア）に統一

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,32 +1,18 @@
 Rails.application.routes.draw do
-  get "job_applications/create"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "health_check" => "rails/health#show"
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 
   namespace :api do
     namespace :v1 do
       get "test/ping"
+      post "test/ping", to: "test#ping"
       post "/login", to: "sessions#create"
       delete "/logout", to: "sessions#destroy"
-      post "test/ping", to: "test#ping"
       get "/me", to: "sessions#me"
 
       resources :companies, only: [ :create ]
-      resources :jobs, only: [ :index, :create ] do
-        resources :applications, only: [ :create ]
-      end
-      resources :applications, only: [ :index, :update, :show ]
+      resources :jobs, only: [ :index, :create ]
+      resources :job_applications, only: [ :index, :create, :show, :update ]
     end
   end
-
-  resources :jobs do
-  end
-  resources :job_applications, only: [ :index, :create, :show, :update ]
 end
