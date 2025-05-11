5e05ba3df8e25145e242088216802d46b3414a0f
jikuya <jikuya@gmail.com>
2025-05-11 12:53:42 +0900

    fix: update ActionMailer default_from configuration for Rails 8.0.2

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'rvf-f1cc04378d39.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_from = 'noreply@rvf-f1cc04378d39.herokuapp.com'
+  config.action_mailer.default_options = { from: 'noreply@rvf-f1cc04378d39.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address: ENV['MAILGUN_SMTP_SERVER'],
     port: ENV['MAILGUN_SMTP_PORT'],
